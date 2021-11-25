931be1d69911051ac45d15acbfe704598eebe1df
Gernot H. Reichel <gernot.reichel@me.com>
2021-11-25 10:47:57 +0100

    implemented Ping Monitor

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ const FritzboxMonitor = require('./lib/fritz-monitor');
 const VeitsbronnTempMonitor = require('./lib/veitsbronntemp-monitor');
 const InfluxDBHandler = require('./lib/influxdb-handler');
 const MQTTHander = require('./lib/mqtt-handler');
+const PingMonitor = require('./lib/ping-monitor');
 
 run = async () => {
     const influxDBHandler = new InfluxDBHandler();
     const mqttHandler = new MQTTHander();
 
-    new FritzboxMonitor([influxDBHandler, mqttHandler]).run();
-    new RaspiTempMonitor([influxDBHandler, mqttHandler]).run();
-    new VeitsbronnTempMonitor([influxDBHandler, mqttHandler]).run();
+//    new FritzboxMonitor([influxDBHandler, mqttHandler]).run();
+//    new RaspiTempMonitor([influxDBHandler, mqttHandler]).run();
+//    new VeitsbronnTempMonitor([influxDBHandler, mqttHandler]).run();
+    new PingMonitor([influxDBHandler, mqttHandler]).run();
 };
 
 run();
