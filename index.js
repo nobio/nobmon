ff3e5e4993516eefdd72cffc9a0ae7c3f65c4236
Gernot H. Reichel <gernot.reichel@me.com>
2022-04-08 17:05:56 +0200

    added wind direction, clouds and rain

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const RaspiTempMonitor = require('./lib/monitor/raspitemp-monitor');
 const FritzboxMonitor = require('./lib/monitor/fritz-monitor');
-const VeitsbronnTempMonitor = require('./lib/monitor/veitsbronntemp-monitor');
+const VeitsbronnWeatherMonitor = require('./lib/monitor/veitsbronn-weather-monitor');
 const PingMonitor = require('./lib/monitor/ping-monitor');
 const InfluxDBHandler = require('./lib/handler/influxdb-handler');
 const MQTTHander = require('./lib/handler/mqtt-handler');
@@ -15,7 +15,7 @@ run = async () => {
 
     new FritzboxMonitor([influxDBHandler, mqttHandler]).run();
     new RaspiTempMonitor([influxDBHandler, mqttHandler]).run();
-    new VeitsbronnTempMonitor([influxDBHandler, mqttHandler]).run();
+    new VeitsbronnWeatherMonitor([influxDBHandler, mqttHandler]).run();
     new PingMonitor([influxDBHandler, mqttHandler]).run();
     //new CPUMonitor([influxDBHandler, mqttHandler]).run();
 };
