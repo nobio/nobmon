b53e3c2e2eb066924e62f0b7f206406b70518950
Gernot H. Reichel <gernot.reichel@me.com>
2022-05-07 13:23:45 +0200

    added linter

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ const CPUMonitor = require('./lib/monitor/cpu-monitor');
 const MemoryMonitor = require('./lib/monitor/mem-monitor');
 
 run = async () => {
-    const influxDBHandler = new InfluxDBHandler();
-    const mqttHandler = new MQTTHander();
+  const influxDBHandler = new InfluxDBHandler();
+  const mqttHandler = new MQTTHander();
 
-    new FritzboxMonitor([influxDBHandler, mqttHandler]).run();
-    new RaspiTempMonitor([influxDBHandler, mqttHandler]).run();
-    new VeitsbronnWeatherMonitor([influxDBHandler, mqttHandler]).run();
-    new PingMonitor([influxDBHandler, mqttHandler]).run();
-    new CPUMonitor([influxDBHandler, mqttHandler]).run();
-    new MemoryMonitor([influxDBHandler, mqttHandler]).run();
+  new FritzboxMonitor([influxDBHandler, mqttHandler]).run();
+  new RaspiTempMonitor([influxDBHandler, mqttHandler]).run();
+  new VeitsbronnWeatherMonitor([influxDBHandler, mqttHandler]).run();
+  new PingMonitor([influxDBHandler, mqttHandler]).run();
+  new CPUMonitor([influxDBHandler, mqttHandler]).run();
+  new MemoryMonitor([influxDBHandler, mqttHandler]).run();
 };
 
 run();
