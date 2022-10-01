f63bc26675aa826906fe12e615526e2dc6fe4080
Gernot H. Reichel <gernot.reichel@me.com>
2022-10-01 13:24:07 +0200

    added nmap count for devices in my network

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const MQTTHander = require('./lib/handler/mqtt-handler');
 const NOPHander = require('./lib/handler/nop-handler');
 const CPUMonitor = require('./lib/monitor/cpu-monitor');
 const MemoryMonitor = require('./lib/monitor/mem-monitor');
+const NMapMonitor = require('./lib/monitor/nmap-monitor');
 
 run = async () => {
     const influxDBHandler = new InfluxDBHandler();
@@ -23,6 +24,8 @@ run = async () => {
         ['PING MONITOR', process.env.PING_MONITOR],
         ['CPU MONITOR', process.env.CPU_MONITOR],
         ['MEMORY MONITOR', process.env.MEMORY_MONITOR],
+        ['NMAP MONITOR', process.env.NMAP_MONITOR],
+
     ]);
 
     if (process.env.FRITZBOX_MONITOR === 'true') new FritzboxMonitor([influxDBHandler, mqttHandler]).run();
@@ -31,6 +34,7 @@ run = async () => {
     if (process.env.PING_MONITOR === 'true') new PingMonitor([influxDBHandler, mqttHandler]).run();
     if (process.env.CPU_MONITOR === 'true') new CPUMonitor([influxDBHandler, mqttHandler]).run();
     if (process.env.MEMORY_MONITOR === 'true') new MemoryMonitor([influxDBHandler, mqttHandler]).run();
+    if (process.env.NMAP_MONITOR === 'true') new NMapMonitor([influxDBHandler, mqttHandler]).run();
 };
 
 run();
